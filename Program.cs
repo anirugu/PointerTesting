56504207225d64c3635bd2161f87acafb2e9f891
Anirudha Gupta <me@anirudhagupta.com>
2022-10-16 07:59:38 +0530

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿public class ListNode
+{
+    public int val;
+    public ListNode next;
+    public ListNode(int val = 0, ListNode next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+
+
+
+namespace PointerTesting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+        }
+    }
+}
